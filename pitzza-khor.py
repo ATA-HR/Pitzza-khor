fecc0841455035f67a0ae2be5915e916a722e251
HamidReza <ataei1564@gmail.com>
2024-07-13 17:30:28 +0330

    now we can pass the wall and obstacles get real

--- pitzza-khor.py
+++ pitzza-khor.py
@@ -13,34 +13,44 @@ plyr_line = plyr_col = 0
 
 def init():
     global plyr_line, plyr_col
-    for i in range(max_line):
+    for i in range(-1, max_line+1):
         map.append([])
-        for j in range(max_col):
-            map[i].append(' ' if random.random() > 0.02 else '.')
+        for j in range(-1, max_col+1):
+            map[i].append(' ' if random.random() > 0.005 else '.')
         
     plyr_col = random.randint(0, max_col)
     plyr_line = random.randint(0, max_line)
 
+def in_range(a, min, max):
+    if a > max:
+        a = min
+    elif a < min:
+        a = max
+    return a
+
 def draw():
     for i in range(max_line):
-        for j in range(max_col):
+        for j in range(max_col): 
             stdscr.addch(i, j, map[i][j])
             
-        stdscr.addch(plyr_line, plyr_col, 'X')
-        stdscr.refresh()
+    stdscr.addch(plyr_line, plyr_col, 'X')
+    stdscr.refresh()
 
 def move(c):
     global plyr_line, plyr_col
-    if c == 'w':
+
+    if c == 'w' and map[plyr_line - 1][plyr_col] != '.':
         plyr_line -= 1
-        if plyr_line == 0:
-            plyr_line = max_line
-    elif c == 's':
+    elif c == 's' and map[plyr_line + 1][plyr_col] != '.':
         plyr_line += 1
-    elif c == 'a':
+    elif c == 'a' and map[plyr_line][plyr_col - 1] != '.':
         plyr_col -= 1
-    elif c == 'd':
+    elif c == 'd' and map[plyr_line][plyr_col + 1] != '.':
         plyr_col += 1
+
+    plyr_line = in_range(plyr_line, 0, max_line)
+    plyr_col = in_range(plyr_col, 0, max_col)
+
 
 init()
 
